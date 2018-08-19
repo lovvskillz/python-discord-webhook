d4c5b0fa911dcd10e76c1878222f3b7ad886fa85
vadim <vadim17@hotmail.de>
2018-08-19 19:30:09 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='discord-webhook',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='execute discord webhooks',
+    long_description=README,
+    url='https://github.com/lovvskillz/python-discord-webhook',
+    install_requires=[
+            'requests>=2.19.1',
+    ],
+    author='Vadim Zifra',
+    author_email='vadim@minehub.de',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
