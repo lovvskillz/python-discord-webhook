988837afe11f658b6fdd452bfbfe49824d29c349
vadim <vadim17@hotmail.de>
2018-08-20 12:28:27 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='discord-webhook',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
