d63e3f4f75b61c238f5152286dedaf7df99a0a4c
vadim <vadim17@hotmail.de>
2019-12-13 00:29:03 +0100

    add example for multiple webhook urls

---
+++ examples/multiple_webhook_urls.py
@@ -0,0 +1,5 @@
+from discord_webhook import DiscordWebhook
+
+webhook_urls = ['webhook url 1', 'webhook url 2']
+webhook = DiscordWebhook(url=webhook_urls, content='Webhook Message')
+response = webhook.execute()
