fc25b2fdbb142e36231070530b69772bd7dc0b9b
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-03-05 20:26:30 +0000

    Added type hints everywhere

--- discord_webhook/webhook_exceptions.py
+++ discord_webhook/webhook_exceptions.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class ColourNotInRangeException(Exception):
     """
     A valid colour must take an integer value between 0 and 16777216 inclusive
@@ -5,10 +8,15 @@ class ColourNotInRangeException(Exception):
     This Exception will be raised when a colour is not in that range.
     """
 
-    def __init__(self, color):
-        self.color = color
+    color: Union[str, int]
 
-    def __str__(self):
-        return repr('"{}" is not in valid range of colors. The valid ranges '
-                    'of colors are 0 to 16777215 inclusive (INTEGERS) and 0 '
-                    'to FFFFFF inclusive (HEXADECIMAL)'.format(self.color))
+    def __init__(self, color: Union[str, int]) -> None:
+        self.color = color
+        super().__init__()
+
+    def __str__(self) -> str:
+        return repr(
+            f"{self.color!r} is not in valid range of colors. The valid ranges "
+            "of colors are 0 to 16777215 inclusive (INTEGERS) and 0 "
+            "to FFFFFF inclusive (HEXADECIMAL)"
+        )
