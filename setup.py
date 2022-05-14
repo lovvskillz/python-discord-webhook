fab143e2ad2e04b730c0c1548936798306a0075c
vadim <vadim17@hotmail.de>
2022-05-14 03:07:17 +0200

    bump version to 0.16.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="discord-webhook",
-    version="0.16.2",
+    version="0.16.3",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
