186042e901ee48f3c6f07e7891d3e72d0ba339be
lovvskillz <14542790+lovvskillz@users.noreply.github.com>
2025-03-01 14:32:36 +0100

    use one webhook url in cli

--- discord_webhook/__main__.py
+++ discord_webhook/__main__.py
@@ -9,13 +9,7 @@ def main() -> bool:
     parser = argparse.ArgumentParser(
         prog="discord_webhook", description="Trigger discord webhook(s)."
     )
-    parser.add_argument(
-        "-u",
-        "--url",
-        required=True,
-        nargs="+",
-        help="Webhook(s) url(s)",
-    )
+    parser.add_argument("-u", "--url", required=True, help="Webhook URL")
     parser.add_argument("-c", "--content", required=True, help="Message content")
     parser.add_argument(
         "--username", default=None, help="override the default username of the webhook"
